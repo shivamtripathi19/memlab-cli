00a3115dad702e8838f0d87355d49d25123a4df5
shivamtripathi19 <shivam.tripathi@tokopedia.com>
2023-02-22 11:35:36 +0530

    feat(poc): logic added

--- utilities/onlyForRoute.js
+++ utilities/onlyForRoute.js
@@ -1,31 +1,48 @@
-const { findLeaks, takeSnapshots } = require("@memlab/api");
-
+const { run } = require("@memlab/api");
 module.exports = async function getOnlyRouteLeak(route) {
+  _puppeteerConfig={
+    headless: false, defaultViewport: null, args:['--start-maximized']
+  }
   const scenario = {
     url: () => `https://cm-staging.tokopedia.net/${route}`,
-    action: async (page) => {
+    beforeInitialPageLoad: async (page) => {
       await page.evaluate(() => {
         localStorage.setItem(
-          "userEntity",
-          JSON.stringify({
-            Id: "",
-            Name: "",
-            profileUrl: "",
-          })
-        );
+          "userEntity", '');
       });
       await page.evaluate(() => {
-        localStorage.setItem("authToken", "");
+        localStorage.setItem(
+          "authToken", '');
       });
       await page.evaluate(() => {
         localStorage.setItem("emailID", "");
       });
       await page.evaluate(() => {
-        localStorage.setItem("permission", true);
+        localStorage.setItem("permission", '');
       });
     },
+    action: async (page) => {
+      await page.click('[data-testid="btnMenuAnalyze"]');
+      await page.click('a[href="/performance"]');
+    },
+    back: async (page) => {
+      await page.click('[data-testid="btnMenuCampaign"]');
+      await page.click('a[href="/campaign-list"]');
+    },
   };
-  const result = await takeSnapshots({ scenario });
-  const leaks = findLeaks(result);
-  console.log(leaks);
+ 
+  const { takeSnapshots } = await run({
+    scenario,
+    snapshotForEachStep: true,
+    workDir: "/Users/shivammanitripathi/Desktop/cli/cmds",
+  });
+
+  // const result = await takeSnapshots({scenario});
+  // const steps = result.getInteractionSteps();
+  // // print each browser interaction's name and JavaScript heap size (in bytes)
+  // steps.forEach((step) => console.log(step.name, step.JSHeapUsedSize));
+
+  // const dataDir = result.getSnapshotFileDir(); // where the last memlab run stores results
+  // const reader = BrowserInteractionResultReader.from(dataDir);
+  // console.log(reader);
 };
